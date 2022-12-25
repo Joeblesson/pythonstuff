1ca9a0d89bdf4bc5bb2a763049e781471a8d6443
Joeblesson <johanblesson@gmail.com>
2022-12-25 22:20:19 +0100

    haha funny joke i have achieved comedy

---
+++ deez.py
@@ -0,0 +1,13 @@
+# -er
+
+first_name = "Daniel"
+last_name = "Marhely"
+age = 38
+has_been_to_deez_hospital_before = False
+
+print('patient profile\n')
+print('Full Name = Daniel Marhely')
+print('Age =', age)
+print('Has been here:', has_been_to_deez_hospital_before)
+
+# a sudden twist 
---
+++ variables.py
@@ -0,0 +1,16 @@
+age = 3
+
+print(age)
+
+age = 30
+
+print(age)
+
+price = 19.95
+
+first_name = "Joeblesson"
+
+is_online = False
+
+print(first_name, price, age)
+print("Is online =", is_online)
